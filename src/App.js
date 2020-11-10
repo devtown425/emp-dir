e75b4900dc71c5d3a78cc359104f2f4626024387
hao guan <haoguan200@gmail.com>
2020-11-09 21:27:31 -0800

    deploy v

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import EmployeeList from './components/EmployeeList'
+import SearchBar from './components/SearchBar'
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  function handleSearch (event) {
+    event.preventDefault();
+    setQuery(event.target.value.trim());
+  }
+
+  function clearSearch (event) {
+    if (event.currentTarget.parentElement.search) {
+      event.currentTarget.parentElement.search.value = "";
+      setQuery("");
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar 
+        handleSearch={handleSearch}
+        clearSearch={clearSearch}
+      />
+      <EmployeeList query={query} />
     </div>
   );
 }
